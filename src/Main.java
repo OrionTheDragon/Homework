1284273f9a494f437c7a3041bb0d146efc1e204e
OrionTheDragon <golub.kd@mail.ru>
2026-04-05 12:06:20 +0300

    выполнил задание

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,82 @@ public class Main {
 //            System.out.println("i = " + i);
 //        }
 
-        var student = 0;
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println("Собака: " + dog +
+                "\nКот: " + cat +
+                "\nБумага: " + paper);
+
+        var plusDog = dog + 4;
+        var plusCat = cat + 4;
+        var plusPaper = paper + 4;
+
+        System.out.println();
+
+        System.out.println("Собака: " + plusDog +
+                "\nКот: " + plusCat +
+                "\nБумага: " + plusPaper);
+
+        System.out.println();
+
+        var minusDog = dog - 3.5;
+        var minusCat = cat - 1.6;
+        var minusPaper = paper - 7639;
+
+        System.out.println("Собака: " + minusDog +
+                "\nКот: " + minusCat +
+                "\nБумага: " + minusPaper);
+
+        System.out.println();
+
+        System.out.println("------ Друг ------");
+        var friend = 19;
+        System.out.println("Друг: " + friend);
+        friend += 2;
+        System.out.println("Друг: " + friend);
+        friend /= 7;
+        System.out.println("Друг: " + friend);
+
+        System.out.println();
+
+        System.out.println("------ Лягушка ------");
+        var frog = 3.5;
+        System.out.println("Лягушка: " + frog);
+        frog *= 10;
+        System.out.println("Лягушка: " + frog);
+        frog /= 3.5;
+        System.out.println("Лягушка: " + frog);
+        frog += 4;
+        System.out.println("Лягушка: " + frog);
+
+        System.out.println();
+
+        System.out.println("------ Бокс ------");
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+
+        var generalWeight = boxer1 + boxer2;
+        System.out.println("Общая масса: " + generalWeight);
+
+        var difference = boxer2 - boxer1;
+        System.out.println("Разница в весе: " + difference);
+
+        var remainderWeight = boxer2 % boxer1;
+        System.out.println("Остаток: " + remainderWeight);
+
+        System.out.println();
+
+        System.out.println("------ Работники ------");
+        var workingHours = 640;
+        var numberEmployees = workingHours / 8;
+        System.out.println("Всего работников в компании - " + numberEmployees + " человек.");
+
+        var newNumberEmployees = numberEmployees + 94;
+        var newWorkingHours = newNumberEmployees * 8;
+        System.out.println("Если в компании работает " + newNumberEmployees +
+                " человек, то всего " + newWorkingHours +
+                " часов работы может быть поделено между сотрудниками.");
     }
 }

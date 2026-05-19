174c012df240e9b87f2a0e331be5a1327445db0d
OrionTheDragon <golub.kd@mail.ru>
2026-05-19 18:44:25 +0300

    Строки

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,12 @@ public class Main {
     public static void main(String[] args) {
         // ================= Задача 1 =================
         System.out.println("Задача 1:");
-        StringBuilder firstName = new StringBuilder();
-        StringBuilder middleName = new StringBuilder();
-        StringBuilder lastName = new StringBuilder();
-        StringBuilder fullName = new StringBuilder();
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
+        String lastName = "Ivanovich";
+        String fullName;
 
-        firstName.append("Ivan");
-        middleName.append("Ivanov");
-        lastName.append("Ivanovich");
-        fullName.append(middleName + " ")
-                .append(firstName + " ")
-                .append(lastName + ".");
+        fullName = middleName + " " + firstName + " " + lastName;
 
         System.out.println("Ф. И. О. сотрудника — " + fullName);
 
@@ -20,19 +15,14 @@ public class Main {
 
         // ================= Задача 2 =================
         System.out.println("Задача 2:");
-        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toString().toUpperCase());
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
 
         System.out.println();
 
         // ================= Задача 3 =================
         System.out.println("Задача 3:");
-        fullName.setLength(0);
-        fullName.append("Иванов Семён Семёнович");
-        for (int i = 0; i < fullName.length(); i++) {
-            if (fullName.charAt(i) == 'ё') {
-                fullName.setCharAt(i, 'е');
-            }
-        }
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
 
         System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
     }

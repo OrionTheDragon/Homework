fe7bf25135eade385ed7cd42144c25f6e4912ef5
OrionTheDragon <golub.kd@mail.ru>
2026-05-26 21:21:34 +0300

    Методы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.time.LocalDate;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 1");
@@ -8,64 +6,51 @@ public class Main {
         System.out.println();
 
         System.out.println("Задача 2");
-        recommendAppVersion(0, 2015);
+        recommendAppVersion((int) (Math.random() * 2), 2015);
 
         System.out.println();
 
         System.out.println("Задача 3");
         int deliveryDistance = (int) (Math.random() * 150) + 1;
-        int deliveryDays = calculateDeliveryDays(deliveryDistance);
-        if (deliveryDays > 0) {
-            System.out.println("Потребуется дней: " + deliveryDays);
-        }
-        else {
-            System.out.println("Доставки нет");
-        }
+
+        String result = calculateDeliveryDays(deliveryDistance);
+        System.out.println(result);
     }
 
     // -------------------- Задача 1 --------------------
     public static void checkLeapYear(int year) {
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
             System.out.println(year + " год — високосный год");
-        }
-        else {
+        } else {
             System.out.println(year + " год — невисокосный год");
         }
     }
 
     // -------------------- Задача 2 --------------------
     public static void recommendAppVersion(int osType, int clientDeviceYear) {
-        if (osType == 0) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            }
-            else {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }
-        }
-        else if (osType == 1) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            }
-            else {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
+        if (osType == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (osType == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (osType == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (osType == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
 
     // -------------------- Задача 3 --------------------
-    public static int calculateDeliveryDays(int distance) {
+    public static String calculateDeliveryDays(int distance) {
+        if (distance < 0 || distance > 100) {
+            return "Доставки нет";
+        }
+
         if (distance <= 20) {
-            return 1;
-        }
-        else if (distance <= 60) {
-            return 2;
-        }
-        else if (distance <= 100) {
-            return 3;
-        }
-        else {
-            return -1;
+            return "Потребуется дней: 1";
+        } else if (distance <= 60) {
+            return "Потребуется дней: 2";
+        } else {
+            return "Потребуется дней: 3";
         }
     }
 }

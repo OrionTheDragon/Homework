32ce1cbd2e58536a45c6eae70471a18ab5a4775e
OrionTheDragon <golub.kd@mail.ru>
2026-05-26 14:40:32 +0300

    Объекты и классы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,62 @@
 public class Main {
     public static void main(String[] args) {
+        Author author1 = new Author("Лев", "Толстой");
+        Author author2 = new Author("Федор", "Достоевский");
 
+        Book book1 = new Book("Война и мир", author1, 1869);
+        Book book2 = new Book("Преступление и наказание", author2, 1866);
+
+        book1.setPublicationYear(1873);
+
+        System.out.println(book1.getTitle() + ", автор: " + book1.getAuthor().getFirstName() + " "
+                + book1.getAuthor().getLastName() + ", " + "год: " + book1.getPublicationYear());
+    }
+}
+
+// Класс Author
+class Author {
+    private String firstName;
+    private String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
+
+// Класс Book
+class Book {
+    private String title;
+    private Author author;
+    private int publicationYear;
+
+    public Book(String title, Author author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
     }
 }
